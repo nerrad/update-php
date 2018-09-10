3a1b9cc6f89a27d7beda811130b7ce3b2d00d62a
Darren Ethier <darren@roughsmootheng.in>
2018-09-09 21:28:09 -0400

    add languages folder (with converted pot file)

---
+++ languages/wp-update-php-js-pot.php
@@ -0,0 +1,40 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: assets/src/blocks/version-detector-content/block.js:112
+	__( 'Outdated PHP Version Content', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:113
+	__( 'Up-to-date PHP Version Content', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:124
+	__( 'When the detected php version is less than the "Minimum Up To Date Version", this is the content that gets shown.', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:128
+	__( 'When the detected php version is equal to or greater than the "Minimum Up To Date Version", this is the content that gets shown.', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:149
+	__( 'Title', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:154
+	__( 'Body', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:159
+	__( 'Emphasis', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:202
+	__( 'Preview Outdated Content', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:205
+	__( 'Use this to switch between previewing the outdated or up-to-date php version content', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:211
+	__( 'Minimum Up To Date Version:', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/block.js:212
+	__( 'The minimum version that will trigger the up-to-date content to show.  Anything below this version will result in out-dated version content showing', 'update-php' ),
+
+	// Reference: assets/src/blocks/version-detector-content/index.js:12
+	__( 'PHP Version Detection Content', 'update-php' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
